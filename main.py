18c160d1c073fc9493893ff353427dc9a1333054
Лоторев Кирилл <151776260+KirillCodeCreator@users.noreply.github.com>
2024-02-24 14:47:15 +0300

    added code for double-security task (#4)
    Co-authored-by: Kirill <okaydiscord@mail.ru>

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import os
 
 from flask import Flask, render_template, redirect, request
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, FileField
+from wtforms import SubmitField, FileField, StringField, PasswordField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class LoginForm(FlaskForm):
+    username_astro = StringField('Id астронавта', validators=[DataRequired()])
+    password_astro = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    username_cap = StringField('Id капитана', validators=[DataRequired()])
+    password_cap = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Войти')
 
 
 class GalleryForm(FlaskForm):
@@ -20,6 +28,19 @@ class GalleryForm(FlaskForm):
 @app.route('/index/<title>')
 def index(title):
     return render_template('index.html', title=title)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 
 @app.route('/table/<sex>/<int:age>')
@@ -45,11 +66,13 @@ def gallery():
             f.write(file.stream.read())
         return redirect('/gallery')
 
+
 @app.route('/member')
 def member():
     with open('templates/members.json', 'r', encoding='utf8') as f:
         members = json.load(f)['members']
     return render_template('member.html', members=members)
+
 
 if __name__ == '__main__':
     app.run("", 8080)
